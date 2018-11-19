071bf33fbe9957b4ce49da3dcb28ddda5efff852
luoxixi1986 <694845384@qq.com>
2018-11-19 22:28:46 +0800

    v666

---
+++ src/test/java/day03/Test3.java
@@ -0,0 +1,89 @@
+﻿package day03;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test3 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+//    driver = new FirefoxDriver();
+	FirefoxProfile profile = new FirefoxProfile();
+	profile.setPreference("dom.ipc.plugins.enabled", false);
+	driver = new FirefoxDriver(profile);
+  	  
+    baseUrl = "http://localhost/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitled1() throws Exception {
+    driver.get(baseUrl + "/ecshop/upload/index.php");
+    driver.findElement(By.id("keyword")).clear();
+    driver.findElement(By.id("keyword")).sendKeys("a");
+    driver.findElement(By.name("imageField")).click();
+    assertEquals("4", driver.findElement(By.cssSelector("b")).getText());
+  }
+  
+  @Test
+  public void testUntitled2() throws Exception {
+    driver.get(baseUrl + "/ecshop/upload/index.php");
+    driver.findElement(By.id("keyword")).clear();
+    driver.findElement(By.id("keyword")).sendKeys("b");
+    driver.findElement(By.name("imageField")).click();
+    assertEquals("1", driver.findElement(By.cssSelector("b")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
